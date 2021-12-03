4460cfe531d56b53715e356dbb881e679f1f4375
쳬 <silverrain0501@gmail.com>
2021-12-03 07:16:44 +0000

    [modify] Github Issue 생성 설정

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import os
+import datetime
 from crawling_post import extract_post_title
+from github_utils import get_github_repo, upload_github_issue
+
+
+access_token = os.environ['TIL_TOKEN']
+repo_name = "Today-I-Learned"
+
 urls = ["https://ffoorreeuunn.tistory.com"]
 
-print(extract_post_title(urls))
+today = datetime.datetime.today().strftime("%Y.%m.%d")
+issue_title = f"TIL(Today I Learned) : {today}"
+issue_contents = extract_post_title(urls)
+repository = get_github_repo(access_token, repo_name)
+upload_github_issue(repository, issue_title, issue_contents)
