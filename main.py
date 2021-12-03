43f6987ce4a2e445618eb7ece8f67e5bd4dabea1
쳬 <silverrain0501@gmail.com>
2021-12-03 07:35:34 +0000

    [modify] main 주석 삭제

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from crawling_post import extract_post_title
 from github import Github
 
 
-# access_token = os.environ['TIL_TOKEN']
-# repo_name = "Today-I-Learned"
+access_token = os.environ['TIL_TOKEN']
+repo_name = "Today-I-Learned"
 
-# urls = ["https://ffoorreeuunn.tistory.com"]
+urls = ["https://ffoorreeuunn.tistory.com"]
 
-# today = datetime.datetime.today().strftime("%Y.%m.%d")
-# issue_title = f"TIL(Today I Learned) : {today}"
-# issue_contents = extract_post_title(urls)
-# repository = Github(access_token).get_user().get_repo(repo_name)
-# repo.create_issue(title=issue_title, body = issue_contents)
+today = datetime.datetime.today().strftime("%Y.%m.%d")
+issue_title = f"TIL(Today I Learned) : {today}"
+issue_contents = extract_post_title(urls)
+repository = Github(access_token).get_user().get_repo(repo_name)
+repo.create_issue(title=issue_title, body = issue_contents)
